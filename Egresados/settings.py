a40d7fbe3e448fe4c39b05725420e666ec573ab7
alka653 <alka65@hotmail.com>
2018-09-08 19:57:18 -0500

    make it better

--- Egresados/settings.py
+++ Egresados/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,10 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'static'
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'Egresados/static')
 
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR,'Egresados/static'),
-	)
+	os.path.join(BASE_DIR, 'Egresados/static'),
+)
